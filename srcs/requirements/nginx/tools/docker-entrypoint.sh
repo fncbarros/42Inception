90c22e53a14d4eafc4ada3f4b3fcb9f5d20145f4
fbarros <fbarros@student.42lisboa.com>
2023-03-10 21:28:37 +0000

    added domain to /etc/hosts

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -8,5 +8,7 @@ mkdir /etc/nginx/certs
 openssl req -x509 -nodes -days 365 -subj "/C=PT/ST=Lisbon/O=42Lisboa/CN=fbarros.42.fr" \
  -addext "subjectAltName=DNS:fbarros.42.fr" -newkey rsa:2048 -keyout /etc/nginx/certs/fbarros.42.fr.key -out /etc/nginx/certs/fbarros.42.fr.crt
 
+echo "127.0.0.1    fbarros.42.fr" >> /etc/hosts
+
 # refer to cheatsheet.txt to know why this is important
 exec "$@"
