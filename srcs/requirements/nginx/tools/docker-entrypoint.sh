58b83ccdba16bc82179a9cf79a9a5ce1d43b7968
fbarros <fbarros@student.42lisboa.com>
2023-03-13 11:08:00 +0000

    minor changes

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mkdir etc/nginx/certs
+# breaking link to default config
+unlink /etc/nginx/sites-enabled/default
+
+mkdir /etc/nginx/certs
 # Generate self signed ssl certificate
 openssl req -x509 -nodes -days 365 -subj "/C=PT/ST=Lisbon/O=42Lisboa/CN=fbarros.42.fr" \
  -addext "subjectAltName=DNS:fbarros.42.fr" -newkey rsa:2048 -keyout /etc/nginx/certs/fbarros.42.fr.key -out /etc/nginx/certs/fbarros.42.fr.crt
 
 # refer to cheatsheet.txt to know why this is important
-exec $@
+exec "$@"
