cc6baf3fb5d97d23b92f344fc65d3baf5c9baeec
fbarros <fbarros@student.42lisboa.com>
2023-03-19 14:46:59 +0000

    creating user for mysql

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-<< EOF | mysql_secure_installation
+user=$MYSQL_USER
+password=$MYSQL_PASSWORD
+db=$MYSQL_DATABASE
+host=localhost
+#host='%'
+
+# Start the secure installation
+<< EOF
 
 Y
 Y
 Y
 Y
-EOF
+EOF | mysql_secure_installation
+
+# Create database and user with set permissions
+commands="CREATE DATABASE \`${db}\`; \
+          CREATE USER '${user}'@'${host}' IDENTIFIED BY '${password}'; \
+          GRANT USAGE ON *.* TO '${user}'@'${host}'; \
+          GRANT ALL ON \`${db}\`.* TO '${user}'@'${host}'; \
+          FLUSH PRIVILEGES;"
+echo "${commands}" | /usr/bin/mysql -u root -p
 
 exec "$@"
