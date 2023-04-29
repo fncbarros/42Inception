a4eab52dbf4c4ec16f844329bd95aeb76d070729
Francisco Barros <fbarros@student.42lisboa.com>
2023-04-29 16:34:19 +0100

    changed mdb entrypoint

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -3,26 +3,26 @@
 user=$MYSQL_USER
 password=$MYSQL_PASSWORD
 db=$MYSQL_DATABASE
-host=localhost
-#host='%'
+host='%'
 
-# Configure mysql
-mysql -u root << EOF
-DELETE FROM mysql.user WHERE User=''; \ # Delete anonymours users
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); \ # Deny remote root access
+# Configure mysql and create database and user with set permissions
+# Delete anonymours users
+# Deny remote root access
+# Remove test database
+mysql --user=root << EOF
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'; \ # Remove test database
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+CREATE DATABASE \`${db}\`;
+CREATE USER '${user}'@'${host}' IDENTIFIED BY '${password}';
+GRANT ALL ON \`${db}\`.* TO '${user}'@'${host}';
+GRANT USAGE ON *.* TO '${user}'@'${host}';
 FLUSH PRIVILEDGES;
 EOF
 
-# Create database and user with set permissions
-commands="CREATE DATABASE \`${db}\`; \
-          CREATE USER '${user}'@'${host}' IDENTIFIED BY '${password}'; \
-          GRANT USAGE ON *.* TO '${user}'@'${host}'; \
-          GRANT ALL ON \`${db}\`.* TO '${user}'@'${host}'; \
-          FLUSH PRIVILEGES;"
-echo "${commands}" | /usr/bin/mysql -u root
-
+# TMP
 tail -f
+# TMP
 
 exec "$@"
