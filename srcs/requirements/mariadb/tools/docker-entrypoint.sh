c9616108ef614a9f3e77a92df0d22fd4291d9b28
fbarros <fbarros@student.42lisboa.com>
2023-05-06 18:13:01 +0100

    attempt solving mysql problem

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -5,6 +5,12 @@ password=$MYSQL_PASSWORD
 db=$MYSQL_DATABASE
 host='%'
 
+sed -i '/bind-address/s/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mysql_install_db --user=mysql
+
+service mysql start
+
 # Configure mysql and create database and user with set permissions
 # Delete anonymours users
 # Deny remote root access
@@ -14,6 +20,9 @@ DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+EOF
+
+mariadb -uroot << EOF
 CREATE DATABASE \`${db}\`;
 CREATE USER '${user}'@'${host}' IDENTIFIED BY '${password}';
 GRANT ALL ON \`${db}\`.* TO '${user}'@'${host}';
