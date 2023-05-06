c47c485e54e4e58dd1a6f55a82e1866adb82aeda
fbarros <fbarros@student.42lisboa.com>
2023-05-06 12:51:35 +0100

    temporary resolution of nginx default file problem

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # breaking link to default config
-unlink /etc/nginx/sites-enabled/default
+# unlink /etc/nginx/sites-enabled/default # --> replacing default in Dockerfile for now
 
 mkdir /etc/nginx/certs
 # Generate self signed ssl certificate
 openssl req -x509 -nodes -days 365 -subj "/C=PT/ST=Lisbon/O=42Lisboa/CN=fbarros.42.fr" \
  -addext "subjectAltName=DNS:fbarros.42.fr" -newkey rsa:2048 -keyout /etc/nginx/certs/fbarros.42.fr.key -out /etc/nginx/certs/fbarros.42.fr.crt
-
-mv /default /etc/nginx/sites-enabled/
 
 # refer to cheatsheet.txt to know why this is important
 exec "$@"
