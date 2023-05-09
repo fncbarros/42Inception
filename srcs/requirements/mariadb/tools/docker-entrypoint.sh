f090d99cb91bd5f2d4106bfab5d6c3ef3a62fe7d
fbarros <fbarros@student.42lisboa.com>
2023-05-09 21:02:46 +0100

    ...

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+# allow external connection
 sed -ie '/bind-address/s/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -ie 's/port/#port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+service mysql start
+
+# mysql_secure_installation equivalent
+# Configure mysql and create database and user with set permissions
+# Delete anonymours users
+# Deny remote root access
+# Remove test database
+mysql --user=root << EOF
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEDGES;
+EOF
 
 chmod -R 755 /var
 
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-/etc/init.d/mysql start
-
-# Configure mysql and create database and user with set permissions
-# Delete anonymours users
-# Deny remote root access
-# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-# Remove test database
-mysql --user=root << EOF
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEDGES;
-EOF
 
 mariadb -u root --password=$MYSQL_ROOT_PASSWORD << EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -29,7 +33,7 @@ FLUSH PRIVILEDGES;
 EOF
 
 # TMP
-tail -f
+# tail -f
 # TMP
 
 exec "$@"
