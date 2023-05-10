10186212eb0a3cd389c71c427cc464e00a742a4e
fbarros <fbarros@student.42lisboa.com>
2023-05-10 19:52:46 +0100

    Mariadb, running better now

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # allow external connection
-sed -ie '/bind-address/s/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -ie '/bind-address/s/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -ie '/bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -ie 's/port/#port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
 service mysql start
 
@@ -12,6 +15,7 @@ service mysql start
 # Deny remote root access
 # Remove test database
 mysql --user=root << EOF
+UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
@@ -19,10 +23,9 @@ DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEDGES;
 EOF
 
-chmod -R 755 /var
+chown -R 755 /var/lib/mysql
 
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
+if [[! -d var/lib/mysql/mysql]]; then
 
 mariadb -u root --password=$MYSQL_ROOT_PASSWORD << EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -31,6 +34,8 @@ GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 GRANT USAGE ON *.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEDGES;
 EOF
+
+fi
 
 # TMP
 # tail -f
