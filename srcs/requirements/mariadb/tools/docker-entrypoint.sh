8e10fe0ec555861349162d281fab7b9bac6296f8
fbarros <fbarros@student.42lisboa.com>
2023-05-10 21:14:22 +0100

    Mariadb no syntax errors now

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,37 +2,43 @@
 
 # allow external connection
 # sed -ie '/bind-address/s/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -ie '/bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -ie 's/bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -ie 's/port/#port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+fi
+
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 
 service mysql start
 
 # mysql_secure_installation equivalent
+# Configure root password
 # Configure mysql and create database and user with set permissions
 # Delete anonymours users
 # Deny remote root access
 # Remove test database
 mysql --user=root << EOF
 UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEDGES;
+FLUSH PRIVILEGES;
 EOF
 
 chown -R 755 /var/lib/mysql
 
-if [[! -d var/lib/mysql/mysql]]; then
 
 mariadb -u root --password=$MYSQL_ROOT_PASSWORD << EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 GRANT USAGE ON *.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEDGES;
+FLUSH PRIVILEGES;
 EOF
 
 fi
