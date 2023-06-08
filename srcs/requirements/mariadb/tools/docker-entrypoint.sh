0b3df119d8a11a76e5aa3f1467393f98f524f32c
fbarros <fbarros@student.42lisboa.com>
2023-06-08 17:39:36 +0100

    temporary solution to missing mysql database issue

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -23,12 +23,13 @@ service mysql start
 # Configure mysql and create database and user with set permissions
 # Remove test database
 # To unallow root access from  from a host that is not 'localhost', '127.0.0.1', or '::1'
-#   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+# Delete "empty" users
+# DELETE FROM mysql.user WHERE User='';
+# DROP DATABASE IF EXISTS test;
 mysql --user=root << _EOF_
 UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-DROP DATABASE IF EXISTS test;
 FLUSH PRIVILEGES;
 _EOF_
 
@@ -49,45 +50,5 @@ fi
 # TMP
 # tail -f
 # TMP
-
-exec "$@"
-
-
-#!/bin/bash
-
-# allow external connection
-sed -ie '/bind-address/s/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
-
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
-fi
-
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-
-service mysql start
-
-mysql --user=root << _EOF_
-UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-DROP DATABASE IF EXISTS test;
-FLUSH PRIVILEGES;
-_EOF_
-
-chown -R mysql:mysql /var/lib/mysql
-
-mysql -u root --password=$MYSQL_ROOT_PASSWORD << _EOF_
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-GRANT USAGE ON *.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
-_EOF_
-
-service mysql stop
-fi
 
 exec "$@"
