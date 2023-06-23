b5f34e92b9a1ba6fc9a39f81ede0ab6aeff6e32d
fbarros <fbarros@student.42lisboa.com>
2023-06-23 20:21:19 +0100

    added user pw

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" /etc/php/7.3/fpm/pool.d/www.conf
-sed -i "s/MYSQL_USER/$MYSQL_USER/g" /etc/php/7.3/fpm/pool.d/www.conf
-sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i s/MYSQL_DATABASE/${MYSQL_DATABASE}/g /var/www/wordpress/wp-config.php
+sed -i s/MYSQL_USER/${MYSQL_USER}/g /var/www/wordpress/wp-config.php
+sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g /var/www/wordpress/wp-config.php
 
-exec "$@
+exec "$@"
