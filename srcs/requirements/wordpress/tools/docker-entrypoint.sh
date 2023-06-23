26e3cfa742c89fdec90d10efd1f3f7c0ae8d35c6
fbarros <fbarros@student.42lisboa.com>
2023-06-23 21:15:09 +0100

    installing wordpress cli

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-sed -i s/MYSQL_DATABASE/${MYSQL_DATABASE}/g /var/www/wordpress/wp-config.php
-sed -i s/MYSQL_USER/${MYSQL_USER}/g /var/www/wordpress/wp-config.php
-sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g /var/www/wordpress/wp-config.php
+if [ ! -e "/var/www/wordpress/wp-config.php" ]; then
+    sed -i s/MYSQL_DATABASE/${MYSQL_DATABASE}/g /var/www/wordpress/wp-config.php
+    sed -i s/MYSQL_USER/${MYSQL_USER}/g /var/www/wordpress/wp-config.php
+    sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g /var/www/wordpress/wp-config.php
+fi
+
+if !(wp core is-installed --alow-root); then
+    wp core install --path=/var/www/wordpress --allow-root --url=${LOGIN}.42.fr --title="inception" --admin_user=${MYSQL_USER} --admin_email=${WP_EMAIL} --admin_password=${MYSQL_PASSWORD}
+    wp user create --path=/var/www/wordpress --allow-root ${MYSQL_USER} ${WP_EMAIL} --user_pass=${MYSQL_PASSWORD}
+fi
 
 exec "$@"
