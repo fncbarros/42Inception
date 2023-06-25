215ef86ddc69a720c3949db95566ebd9726793e8
fbarros <fbarros@student.42lisboa.com>
2023-06-25 19:10:27 +0100

    not doing second config

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -5,10 +5,4 @@ sed -i s/MYSQL_USER/${MYSQL_USER}/g /var/www/wordpress/wp-config.php
 sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g /var/www/wordpress/wp-config.php
 chown -R www-data:www-data /var/www/wordpress
 
-if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
-    wp core install --allow-root --path=/var/www/wordpress --url=${LOGIN}.42.fr --title="inception" --admin_user=${WP_USER} --admin_email=${WP_EMAIL} --skip-email --admin_password=${WP_PASSWORD}
-else
-    echo "wp core intallation was already executed"
-fi
-
 exec "$@"
